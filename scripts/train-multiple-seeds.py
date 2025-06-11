25d8ac158b2e85d8a0f9f42a1e496f34c930c3ed
Sean <trott.sean@gmail.com>
2025-06-11 14:01:08 -0700

    update

--- scripts/train-multiple-seeds.py
+++ scripts/train-multiple-seeds.py
@@ -106,6 +106,7 @@ def train_model(model_config_dict, training_config, run_name, dataname):
                 return (
                     step < 100 and step % 10 == 0 or
                     step <= 1000 and step % 50 == 0 or
+                    step == 2000 or 
                     step % 5000 == 0
                 )
 
